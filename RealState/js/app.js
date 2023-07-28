62a150d7761b9ea8eb06df7e554b5f19732087d9
Carlos May <carlosmay220@gmail.com>
2023-07-27 23:58:05 -0600

    Corregir errores de color e imágenes

--- RealState/js/app.js
+++ RealState/js/app.js
@@ -2,7 +2,7 @@ const imagenes = document.querySelectorAll(".propiedad__imagen");
 
 window.addEventListener("scroll", () => {
 
-    const scroll = this.scrollY / -20;
+    const scroll = this.scrollY / -35;
     imagenes.forEach( (imagen) => {
         imagen.style.backgroundPositionY = `${scroll}px`;
     });
